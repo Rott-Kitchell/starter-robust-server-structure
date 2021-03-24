0f9c5d74a61387a0660a522cb2a60d099f89b53f
RottK <r.kitchell815@gmail.com>
2021-03-24 14:17:50 -0500

    RESTful API prac

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 app.use("/counts/:countId", (req, res, next) => {
   const { countId } = req.params;
@@ -31,13 +32,33 @@ app.use("/flips/:flipId", (req, res, next) => {
 // TODO: Follow instructions in the checkpoint to implement ths API.
 const flips = require("./data/flips-data");
 
-app.use("/flips", (req, res) => {
+app.get("/flips", (req, res) => {
   res.json({ data: flips });
+});
+
+let lastFlipId = flips.reduce((maxId, flip) => Math.max(maxId, flip.id), 0);
+
+app.post("/flips", (req, res, next) => {
+  const { data: { result } = {} } = req.body;
+  if (result) {
+    const newFlip = {
+      id: ++lastFlipId, // Increment last id then assign as the current ID
+      result,
+    };
+    flips.push(newFlip);
+    counts[result] = counts[result] + 1; // Increment the counts
+    res.status(201).json({ data: newFlip });
+  } else {
+    res.sendStatus(400);
+  }
 });
 // Not found handler
 app.use((request, response, next) => {
   next(`Not found: ${request.originalUrl}`);
 });
+
+// Variable to hold the next id.
+// Since some ID's may already be used, you find the largest assigned id.
 
 // Error handler
 app.use((error, request, response, next) => {
