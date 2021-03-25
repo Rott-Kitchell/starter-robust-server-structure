1ee8955ed445b4194587a1b3ec195d5f7758f706
RottK <r.kitchell815@gmail.com>
2021-03-25 16:11:34 -0500

    routes

--- src/app.js
+++ src/app.js
@@ -1,69 +1,40 @@
 const express = require("express");
 const app = express();
+const flipsRouter = require("./flips/flips.router");
+const countsRouter = require("./counts/counts.router");
+const flips = require("./data/flips-data");
+const counts = require("./data/counts-data");
+
 app.use(express.json());
 
-app.use("/counts/:countId", (req, res, next) => {
-  const { countId } = req.params;
+app.get("/counts/:countId", (request, response, next) => {
+  const { countId } = request.params;
   const foundCount = counts[countId];
 
   if (foundCount === undefined) {
-    next(`Count id not found: ${countId}`);
+    next({
+      status: 404,
+      message: `Count id not found: ${countId}`,
+    });
   } else {
-    res.json({ data: foundCount }); // Return a JSON object, not a number.
+    response.json({ data: foundCount });
   }
 });
 
-const counts = require("./data/counts-data");
+app.use("/counts", countsRouter);
 
-app.use("/counts", (req, res) => {
-  res.json({ data: counts });
-});
+app.use("/flips", flipsRouter);
 
-app.use("/flips/:flipId", (req, res, next) => {
-  const { flipId } = req.params;
-  const foundFlip = flips.find((flip) => flip.id === Number(flipId));
-
-  if (foundFlip) {
-    res.json({ data: foundFlip });
-  } else {
-    next(`Flip id not found: ${flipId}`);
-  }
-});
-// TODO: Follow instructions in the checkpoint to implement ths API.
-const flips = require("./data/flips-data");
-
-app.get("/flips", (req, res) => {
-  res.json({ data: flips });
-});
-
-let lastFlipId = flips.reduce((maxId, flip) => Math.max(maxId, flip.id), 0);
-
-app.post("/flips", (req, res, next) => {
-  const { data: { result } = {} } = req.body;
-  if (result) {
-    const newFlip = {
-      id: ++lastFlipId, // Increment last id then assign as the current ID
-      result,
-    };
-    flips.push(newFlip);
-    counts[result] = counts[result] + 1; // Increment the counts
-    res.status(201).json({ data: newFlip });
-  } else {
-    res.sendStatus(400);
-  }
-});
 // Not found handler
 app.use((request, response, next) => {
-  next(`Not found: ${request.originalUrl}`);
+  next({ status: 404, message: `Not found: ${request.originalUrl}` });
 });
-
-// Variable to hold the next id.
-// Since some ID's may already be used, you find the largest assigned id.
 
 // Error handler
 app.use((error, request, response, next) => {
   console.error(error);
-  response.send(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  response.status(status).json({ error: message });
 });
 
 module.exports = app;
